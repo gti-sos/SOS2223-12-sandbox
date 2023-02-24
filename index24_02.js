1fc3a1558595ba11849f55e6f4e1cd89fe7c7ed9
Alvaro Maraver Lerma <alvmarler@alum.us.es>
2023-02-24 17:08:46 +0100

    24_02

--- index24_02.js
+++ index24_02.js
@@ -10,3 +10,16 @@
 
 var cool = require("cool-ascii-faces");
 console.log(cool());
+
+//////////////////////////////////////////////////////////////////////////////////////
+var express = require("express");
+var port = 12345;
+var app = express();
+
+app.get("/faces", (request, response) =>{
+    response.send(cool());
+    console.log("New request");
+});
+
+app.listen(port);
+console.log("Servidor funcionando...");
